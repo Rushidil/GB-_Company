4ef505b757ec47e04f4be88436d8c603276ec5c3
TharushiHimaya <tharushihimaya99@outlook.com>
2021-04-23 01:08:40 +0530

    Add and update function completed

--- ProductManagement/src/com/ItemService.java
+++ ProductManagement/src/com/ItemService.java
@@ -21,6 +21,41 @@ public class ItemService
 	{
 		return itemObj.readItems(); 
 	}
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(
+			@FormParam("ItemCode") String ItemCode,
+			@FormParam("ItemName") String ItemName,
+			@FormParam("Description") String Description,
+			@FormParam("Researcher") String Researcher,
+			@FormParam("Email") String Email)
+	{ 
+		String output = itemObj.insertItem(ItemCode, ItemName, Description, Researcher, Email);
+		return output; 
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateItem(String itemData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String ID = itemObject.get("ItemID").getAsString();
+	 String code = itemObject.get("ItemCode").getAsString();
+	 String name = itemObject.get("ItemName").getAsString();
+	 String desc = itemObject.get("Description").getAsString();
+	 String researcher = itemObject.get("Researcher").getAsString();
+	 String email = itemObject.get("Email").getAsString();
+	 String output = itemObj.updateItem(ID,code, name, desc, researcher, email);
+	return output;
+	}
+	
+	
 	
 	
 
