ed5ef6d27d53bd792984ea3d3e661c8bc351b836
Rushidil <thirushidilmika555@gmail.com>
2021-04-22 20:37:26 +0530

    retrieve total price from cart

--- GB_Purchase_Of_Products/src/model/Order.java
+++ GB_Purchase_Of_Products/src/model/Order.java
@@ -1,9 +1,17 @@
 package model;
 
+
+
 import java.sql.*;
 
 public class Order {
 	
+	private int quantity;
+	private double uPrice;
+	private int foreignKey;
+
+
+
 	//A common method to connect to the DB
 		private Connection connect()
 		 {
@@ -38,17 +46,48 @@ public class Order {
 	     }
 		 // create a prepared statement
 		 
-		 String query =  " insert into orderdetails (`oID`,`date`,`cname`,`phone`,`address`,`email`) "
-		 		         + "values (?, ?, ?, ?, ?, ?)";
+		 String query =  " insert into orderdetails (`oID`,`cID`,`date`,`cname`,`phone`,`address`,`email`,`total`) "
+		 		         + "values (?, ?, ?, ?, ?, ?,?,?)";
+		 
+		 //calculate total amount
+		 String query2 = "select qty,unitPrice from cart order by cID desc limit 1";
+		 PreparedStatement preparedStmt2 = con.prepareStatement(query2);
+		 ResultSet rs = preparedStmt2.executeQuery(query2);
+		
+		while(rs.next()) {
+			 
+			 quantity = (rs.getInt("qty"));
+			 uPrice=  (rs.getDouble("unitPrice"));
+		 }
+		 
+		 String total =  Double.toString(quantity * uPrice);
+		 preparedStmt2.execute();
+		 
+		 //to get cartID as foreign key
+		 String query3 = "select cID from cart order by cID desc limit 1";
+		 PreparedStatement preparedStmt3 = con.prepareStatement(query3);
+		 ResultSet rs3 = preparedStmt3.executeQuery(query3);
+		 
+			while(rs3.next()) {
+				 
+				 foreignKey = (rs3.getInt("cID"));
+				 
+			 }
+			
+		 String cartID =  Integer.toString(foreignKey);
+		 preparedStmt3.execute();
 		 
 		 PreparedStatement preparedStmt = con.prepareStatement(query);
 		 // binding values
 		 preparedStmt.setInt(1, 0);
-		 preparedStmt.setString(2, date);
-		 preparedStmt.setString(3, cname);
-		 preparedStmt.setString(4, phone);
-		 preparedStmt.setString(5, address);
-		 preparedStmt.setString(6, email);
+		 preparedStmt.setString(2, cartID);
+		 preparedStmt.setString(3, date);
+		 preparedStmt.setString(4, cname);
+		 preparedStmt.setString(5, phone);
+		 preparedStmt.setString(6, address);
+		 preparedStmt.setString(7, email);
+		 preparedStmt.setString(8,total);
+		 
 		// execute the statement
 		
 		 preparedStmt.execute();
@@ -80,6 +119,7 @@ public class Order {
 		                  "<th>Phone number</th>" +
 		                  "<th>Address</th>" +
 		                   "<th>Email</th>" +
+		                   "<th>Total Price</th>" +
 		                   "<th>Update</th><th>Remove</th></tr>";
 
 		 String query = "select * from orderdetails";
@@ -94,12 +134,15 @@ public class Order {
 		 String cPhone =rs.getString("phone");
 		 String cAddress = rs.getString("address");
 		 String cEmail=rs.getString("email");
+		 String total = Double.toString( rs.getDouble("total"));
+		 
 		 // Add into the html table
 		 output += "<tr><td>" + oDate + "</td>";
 		 output += "<td>" + cusName + "</td>";
 		 output += "<td>" + cPhone + "</td>";
 		 output += "<td>" + cAddress + "</td>";
 		 output += "<td>" + cEmail + "</td>";
+		 output += "<td>" + total + "</td>";
 		 // buttons
 		 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'> </td>"
 		 + "<td><form method='post' action='items.jsp'>"
