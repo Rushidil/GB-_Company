70e18456c5a83ebb6eeaad517519749fed381dde
saara-IT19165608 <f.saara99@gmail.com>
2021-04-25 17:02:34 +0530

    Adding Update

--- UserMangement/src/com/UserService.java
+++ UserMangement/src/com/UserService.java
@@ -23,5 +23,40 @@ public class UserService
 		return userObj.readItems();
 	}
 	
-
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertUser(
+		@FormParam("userID") String userID,
+		@FormParam("name") String name,
+		@FormParam("email") String email,
+		@FormParam("password") String password,
+		@FormParam("nic") String nic)
+	{
+		String output = userObj.insertUser(userID, name, email, password, nic);
+		return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateUser(String userData)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject itemObject = new JsonParser().parse(userData).getAsJsonObject();
+	//Read the values from the JSON object
+	 String uID = itemObject.get("ID").getAsString();
+	 String UserID = itemObject.get("userID").getAsString();
+	 String Name = itemObject.get("name").getAsString();
+	 String Email = itemObject.get("email").getAsString();
+	 String Password = itemObject.get("password").getAsString();
+	 String Nic = itemObject.get("nic").getAsString();
+	 String output = userObj.updateUser(uID, UserID, Name, Email, Password, Nic);
+	return output;
+	}
+	
+	
+	
 }
