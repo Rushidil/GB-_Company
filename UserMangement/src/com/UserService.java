3c839a84d081075850773b08022b5b9011e0bd19
saara-IT19165608 <f.saara99@gmail.com>
2021-04-25 16:58:52 +0530

    Adding Insert

--- UserMangement/src/com/UserService.java
+++ UserMangement/src/com/UserService.java
@@ -24,38 +24,4 @@ public class UserService
 	}
 	
 
-	
-	@PUT
-	@Path("/")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String updateUser(String userData)
-	{
-	//Convert the input string to a JSON object
-	 JsonObject itemObject = new JsonParser().parse(userData).getAsJsonObject();
-	//Read the values from the JSON object
-	 String uID = itemObject.get("ID").getAsString();
-	 String UserID = itemObject.get("userID").getAsString();
-	 String Name = itemObject.get("name").getAsString();
-	 String Email = itemObject.get("email").getAsString();
-	 String Password = itemObject.get("password").getAsString();
-	 String Nic = itemObject.get("nic").getAsString();
-	 String output = userObj.updateUser(uID, UserID, Name, Email, Password, Nic);
-	return output;
-	}
-	
-	@DELETE
-	@Path("/")
-	@Consumes(MediaType.APPLICATION_XML)
-	@Produces(MediaType.TEXT_PLAIN)
-	public String deleteUser(String userData)
-	{
-	//Convert the input string to an XML document
-	 Document doc = Jsoup.parse(userData, "", Parser.xmlParser());
-
-	//Read the value from the element <itemID>
-	 String ID = doc.select("ID").text();
-	 String output = userObj.deleteUser(ID);
-	return output;
-	}
 }
